f3e81825c01c972d64e3c085a0244f659996df90
lishiheng <532909655@qq.com>
2019-12-15 11:31:06 +0800

    create execute.py

---
+++ TianShuData/KuaiShou/execute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'lish'
+
+from scrapy.cmdline import execute
+
+# execute(['scrapy', 'crawl', 'kuaishou_test'])
+execute(['scrapy', 'crawl', 'kuxuan_kol_user'])
+# execute(['scrapy', 'crawl', 'kuaishou_user_photo_info'])
+# execute(['scrapy', 'crawl', 'kuaishou_user_info'])
+# execute(['scrapy', 'crawl', 'kuaishou_photo_comment'])
+# execute(['scrapy', 'crawl', 'kuaishou_cookie_info'])
