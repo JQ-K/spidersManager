449b5a0bd8541ad4407cdfbc72f39868de3abf08
lishiheng <532909655@qq.com>
2019-12-20 20:33:46 +0800

    去掉代理的注释

--- TianShuData/KuaiShou/KuaiShou/middlewares.py
+++ TianShuData/KuaiShou/KuaiShou/middlewares.py
@@ -90,14 +90,14 @@ class KuaishouDownloaderMiddleware(object):
         if spider.name =='kuxuan_kol_user':
             return None
         # # 设置代理IP
-        # proxy_list = self.conn.srandmember(self.redis_proxyip_name, 1)
-        # while proxy_list == []:
-        #     spider.logger.warn('Proxy Pool is null, Plase add proxy !')
-        #     time.sleep(60)
-        #     proxy_list = self.conn.srandmember(self.redis_proxyip_name, 1)
-        # proxy = proxy_list[0].decode()
-        # spider.logger.info('proxy:{}'.format(proxy))
-        # request.meta['proxy'] = proxy
+        proxy_list = self.conn.srandmember(self.redis_proxyip_name, 1)
+        while proxy_list == []:
+            spider.logger.warn('Proxy Pool is null, Plase add proxy !')
+            time.sleep(60)
+            proxy_list = self.conn.srandmember(self.redis_proxyip_name, 1)
+        proxy = proxy_list[0].decode()
+        spider.logger.info('proxy:{}'.format(proxy))
+        request.meta['proxy'] = proxy
         # 获取cookie不能设置cookie，不然cookie就都是设定的了
         if spider.name in ['kuaishou_cookie_info','kuaishou_register_did']:
             return None
