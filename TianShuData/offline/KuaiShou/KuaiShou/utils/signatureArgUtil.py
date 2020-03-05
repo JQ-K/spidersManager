5f815c8dd703e6cb591fd2e92c679b334717658f
onfdtz <dttbxq@126.com>
2020-03-05 15:17:15 +0800

    update sig file

--- TianShuData/offline/KuaiShou/KuaiShou/utils/signatureArgUtil.py
+++ TianShuData/offline/KuaiShou/KuaiShou/utils/signatureArgUtil.py
@@ -4,7 +4,14 @@ import random
 class signatureArgUtil:
 
     def getMod(self):
-        modList = ['OPPO(OPPO%20R11)', 'Xiaomi%20(MI%206%20)', 'HUAWEI%20(HUAWEI%20MLA-AL10)']
+        modList = ['OPPO(OPPO%20R11)',
+                   'Xiaomi%20(MI%206%20)',
+                   'Xiaomi%20(Mi%20Note%202)',
+                   'HUAWEI%20(HUAWEI%20MLA-AL10)',
+                   'vivo(vivo%20X20)',
+                   'vivo(vivo%20X20A)',
+                   'vivo(vivo%20X9i)',
+                   'Meizu(M6%20Note)', ]
         return random.choice(modList)
 
     def getLon(self):
@@ -21,11 +28,32 @@ class signatureArgUtil:
         decPart = str(random.choice(range(0, 1000000))).zfill(6)
         return intPart + '.' + decPart
 
+    def getDid(self):
+        #模拟did：ANDROID_982abcde39d99009，ANDROID_后跟上16位数字英文组成的字符串
+        preStr = 'ANDROID_'
+        chList = [chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]
+        rltList = [random.choice(chList) for i in range(16)]
+        return preStr + ''.join(rltList)
+
+    def getClientKey(self):
+        #client_key随机模拟结果不可使用
+        #client_key:需要安装5.2版本真实的client_key,使用新版本client_key会提示签名验证失败
+        # {
+        #     "result": 40,
+        #     "error_msg": "服务器繁忙，请稍后再试。"
+        # }
+        # {
+        #     "result": 3,
+        #     "error_msg": "客户端版本太旧，请升级到最新版本。"
+        # }
+        pass
+
 
 if __name__ == '__main__':
     argUtil = signatureArgUtil()
     print(argUtil.getMod())
     print(argUtil.getLon())
     print(argUtil.getLat())
+    print(argUtil.getDid())
 
 
--- TianShuData/offline/KuaiShou/KuaiShou/utils/signatureUtil.py
+++ TianShuData/offline/KuaiShou/KuaiShou/utils/signatureUtil.py
@@ -42,7 +42,11 @@ class signatureUtil():
 
 if __name__ == '__main__':
     sigUtil = signatureUtil()
-    srcStr = "mod=OPPO(OPPO%20R11)&lon=120.174975&country_code=CN&did=ANDROID_982cbccac9d99034&app=0&net=WIFI&oc=UNKNOWN&ud=0&c=ALI_CPD&sys=ANDROID_5.1.1&appver=5.2.1.4686&ftt=&language=zh-cn&lat=30.270968&ver=5.2&client_key=3c2cd3f3&os=android&tagName=快影片场&pcursor=0"
+    #srcStr = "mod=OPPO(OPPO%20R11)&lon=120.174975&country_code=CN&did=ANDROID_982abcde39d99009&app=0&net=WIFI&oc=UNKNOWN&ud=0&c=ALI_CPD&sys=ANDROID_5.1.1&appver=5.2.1.4686&ftt=&language=zh-cn&lat=30.270968&ver=5.2&client_key=56c3713c&os=android&pcursor=0&sig=07e7e9d39e7dc7f798bf649c65df0bac"
+    #srcStr = "mod=OPPO(OPPO%20R11)&lon=120.174975&country_code=CN&did=ANDROID_982abcde39d99009&app=0&net=WIFI&oc=UNKNOWN&ud=0&c=ALI_CPD&sys=ANDROID_5.1.1&appver=5.2.1.4686&ftt=&language=zh-cn&lat=30.270968&ver=5.2&client_key=3c2cd3f3&os=android&pcursor=0&sig=c4108057708b9ffedc2854b85f6c3ced"
+    #srcStr = "mod=OPPO(OPPO%20R11)&lon=120.174975&country_code=CN&did=ANDROID_3jy88vre7strvr8f&app=0&net=WIFI&oc=UNKNOWN&ud=0&c=ALI_CPD&sys=ANDROID_5.1.1&appver=5.2.1.4686&ftt=&language=zh-cn&lat=30.270968&ver=5.2&client_key=3c2cd3f3&os=android&pcursor=0&sig=d676bf38646cfbf989a4c5d244806ecb"
+    #srcStr = "mod=OPPO(OPPO%20R11)&lon=120.174975&country_code=CN&did=ANDROID_33258vre7strvr8f&app=0&net=WIFI&oc=UNKNOWN&ud=0&c=ALI_CPD&sys=ANDROID_5.1.1&appver=5.2.1.4686&ftt=&language=zh-cn&lat=30.270968&ver=5.2&client_key=5aec8372&os=android&pcursor=0&sig=75ddb713e7f6ad869dad3b84cca4839b"
+    srcStr = "mod=OPPO(OPPO%20R11)&lon=120.174975&country_code=CN&did=ANDROID_33258tgr7strvr8f&app=0&net=WIFI&oc=UNKNOWN&ud=0&c=ALI_CPD&sys=ANDROID_5.1.1&appver=5.2.1.4686&ftt=&language=zh-cn&lat=30.270968&ver=5.2&client_key=3c2cd3f3&os=android&pcursor=0&sig=085770968bf52b0ef8c2148be950be73"
     sig = sigUtil.getSig(srcStr)
     print(sig)
 
